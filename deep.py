95fb8149c73fd5500bbc3d68cb3da9c28ce622ce
zTgx <747674262@qq.com>
2025-07-14 12:33:13 +0800

    add deep

---
+++ deep.py
@@ -0,0 +1,41 @@
+import tiktoken
+import torch 
+from torch import nn
+
+class DeepText(nn.Module):
+    def __init__(self, cfg):
+        pass
+        
+    def forward(self, in_index):
+        pass
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+
+    def forward(self, x):
+        return x
+
+class LayerNorm(nn.Module):
+    def __init__(self, normalized_shape, eps=1e-5):
+        super().__init__()
+
+    def forward(self, x):
+        return x
+    
+tokenizer = tiktoken.get_encoding("gpt2")
+batch = []
+text1 = "Every effort moves you"
+text2 = "Every day holds a"
+
+batch.append(torch.tensor(tokenizer.encode(text1, allowed_special={"<|endoftext|>"})))
+batch.append(torch.tensor(tokenizer.encode(text2, allowed_special={"<|endoftext|>"})))
+batch = torch.stack(batch, dim=0)
+print(batch)  # torch.Size([2, 4])
+
+torch.manual_seed(123)
+model = DeepText(cfg=None)
+logits = model(batch)
+print(logits.shape)  # Should match the expected output shape based on the model's architecture
+print(logits)
+

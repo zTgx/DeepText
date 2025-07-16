ea6ca0369ff62d44eb01a3346bd602dccab46823
zTgx <747674262@qq.com>
2025-07-16 12:09:16 +0800

    text -> embedding -> tensor -> forward -> logits

--- deep.py
+++ deep.py
@@ -1,6 +1,6 @@
-import tiktoken
 import torch 
 from torch import nn
+import tiktoken
 
 # Vocabulary size
 # Context length
@@ -21,12 +21,52 @@ GPT_CONFIG_124M = {
     "qkv_bias": False
 }
 
+# DeepText model definition
+# A simplified version of a GPT-like model using PyTorch’s neural network module (nn.Module).
 class DeepText(nn.Module):
     def __init__(self, cfg):
-        pass
+        super().__init__()
+
+        self.cfg = cfg if cfg is not None else GPT_CONFIG_124M
         
+        self.vocab_size = self.cfg["vocab_size"]
+        self.emb_dim = self.cfg["emb_dim"]
+        self.tok_emb = nn.Embedding(self.vocab_size, self.emb_dim)
+
+        self.context_length = self.cfg["context_length"]
+        self.pos_emb = nn.Embedding(self.context_length, self.emb_dim)
+
+        self.drop = self.cfg["drop_rate"]
+        self.drop_emb = nn.Dropout(self.drop)
+
+        self.trf_blocks = nn.Sequential(
+            *[TransformerBlock(self.cfg) for _ in range(self.cfg["n_layers"])]
+        )
+
+        self.final_norm = LayerNorm(self.emb_dim, eps=1e-5)
+
+        self.out_head = nn.Linear(self.emb_dim, self.vocab_size, bias=self.cfg["qkv_bias"])
+
+
+    # Describes the data flow through the model
     def forward(self, in_index):
-        pass
+        batch_size, seq_len = in_index.shape
+        assert seq_len <= self.context_length, "Input sequence length exceeds context length."
+
+        tok_emb = self.tok_emb(in_index)
+        pos_emb = self.pos_emb(torch.arange(seq_len, device=in_index.device))
+
+        x = tok_emb + pos_emb
+
+        # Applies dropout
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+
+        # Logits shape: (batch_size, seq_len, vocab_size)
+        #  每一个 单词计算一个分数（称为 "logit"）
+        return logits
 
 class TransformerBlock(nn.Module):
     def __init__(self, cfg):
@@ -47,12 +87,26 @@ batch = []
 text1 = "Every effort moves you"
 text2 = "Every day holds a"
 
-batch.append(torch.tensor(tokenizer.encode(text1, allowed_special={"<|endoftext|>"})))
-batch.append(torch.tensor(tokenizer.encode(text2, allowed_special={"<|endoftext|>"})))
+text1_tokens = tokenizer.encode(text1, allowed_special={"<|endoftext|>"})
+text2_tokens = tokenizer.encode(text2, allowed_special={"<|endoftext|>"})
+print(f"Text 1 tokens: {text1_tokens}")
+print(f"Text 2 tokens: {text2_tokens}")
+
+text1_tokens_tensor = torch.tensor(text1_tokens)
+text2_tokens_tensor = torch.tensor(text2_tokens)
+print(f"Text 1 tokens tensor: {text1_tokens_tensor}")
+print(f"Text 2 tokens tensor: {text2_tokens_tensor}")
+
+batch.append(text1_tokens_tensor)
+batch.append(text2_tokens_tensor)
+
+# dim=0 means the new dimension will be the very first one.
 batch = torch.stack(batch, dim=0)
 print(batch)  # torch.Size([2, 4])
 
 torch.manual_seed(123)
+
+# Initialize the model and run a forward pass
 model = DeepText(cfg=None)
 logits = model(batch)
 print(logits.shape)  # Should match the expected output shape based on the model's architecture
